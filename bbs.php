22c2ba5126189dd5b5e471f8c60b2bf389e273ff
shinya-nexseed <shirai@nexseed.net>
2016-12-02 16:15:28 +0800

    編集・削除機能追加

--- bbs.php
+++ bbs.php
@@ -6,13 +6,76 @@
   $dbh = new PDO($dsn, $user, $password);
   $dbh->query('SET NAMES utf8');
 
+  // 歯車アイコンクリック時
+  // 変数の初期化
+  $editName = '';
+  $editComment = '';
+  $id = '';
+
+  $btnStr = 'つぶやく';
+  if (!empty($_GET['action']) && $_GET['action'] == 'edit') {
+      // パラメータにactionがあり、かつactionの中身がeditだったら処理
+      $sql = 'SELECT * FROM `posts` WHERE `id` = ?';
+      $data[] = $_GET['id'];
+
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute($data);
+
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+      // 変数に値を格納
+      $editName = $rec['nickname'];
+      $editComment = $rec['comment'];
+      $id = $rec['id'];
+      $btnStr = '更新';
+  }
+
+  // echo '<br>';
+  // echo '<br>';
+  // echo $editName . '<br>';
+  // echo $editComment . '<br>';
+  // echo $id . '<br>';
+
+  // $_GETスーパーグローバル変数
+  // URLの?以下 (パラメータ) をkey=valueの連想配列形式で取得する変数
+  // 複数のパラメータを持つURL : bbs.php?key1=value1&key2=value2&key3=value3
+  // $_GET = array('key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3');
+  // ↑↑↑内部ではPHPが上記の式を処理している
+
+
   // ②ひとことデータの登録処理
   if (!empty($_POST)) { // フォームからPOST送信があった際にのみ実行される
-      // 登録処理用のsql文を作成
-      $sql = 'INSERT INTO `posts` (`nickname`, `comment`, `created`)
-                    VALUES (?, ?, NOW())';
-      $data[] = $_POST['nickname'];
-      $data[] = $_POST['comment'];
+
+      if (!empty($_POST['id'])) {
+          // 更新処理用のsql文を作成
+          $sql = 'UPDATE `posts` SET `nickname` = ?, `comment` = ? WHERE `id` = ?';
+          $data[] = $_POST['nickname'];
+          $data[] = $_POST['comment'];
+          $data[] = $_POST['id'];
+      } else {
+          // 登録処理用のsql文を作成
+          $sql = 'INSERT INTO `posts` (`nickname`, `comment`, `created`)
+                        VALUES (?, ?, NOW())';
+          $data[] = $_POST['nickname'];
+          $data[] = $_POST['comment'];
+      }
+
+      // SQL実行
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute($data);
+
+      header('Location: bbs.php'); // 指定したURLに遷移
+      exit(); // これ以下の処理を停止
+  }
+
+  // データの削除処理
+  if (!empty($_GET['action']) && $_GET['action'] == 'delete') {
+      // 物理削除
+      // $sql = 'DELETE FROM `posts` WHERE `id` = ?';
+
+      // 論理削除 (フラグデータを変更)
+      $sql = 'UPDATE `posts` SET `delete_flag` = 1 WHERE `id` = ?';
+
+      $data[] = $_GET['id'];
 
       // SQL実行
       $stmt = $dbh->prepare($sql);
@@ -23,7 +86,7 @@
   }
 
   // 登録データ一覧表示
-  $sql = 'SELECT * FROM `posts` ORDER BY `created` DESC';
+  $sql = 'SELECT * FROM `posts` WHERE `delete_flag` = 0 ORDER BY `created` DESC';
   // ORDER BY句
     // 指定したカラムの値を元に順番を決めて取得
     // DESC (降順) と ASC (昇順) がある
@@ -107,19 +170,20 @@
           <!-- nickname -->
           <div class="form-group">
             <div class="input-group">
-              <input type="text" name="nickname" class="form-control" id="validate-text" placeholder="nickname" required>
+              <input type="text" name="nickname" class="form-control" id="validate-text" placeholder="nickname" required value="<?php echo $editName; ?>">
               <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
             </div>
           </div>
           <!-- comment -->
           <div class="form-group">
             <div class="input-group" data-validate="length" data-length="4">
-              <textarea type="text" class="form-control" name="comment" id="validate-length" placeholder="comment" required></textarea>
+              <textarea type="text" class="form-control" name="comment" id="validate-length" placeholder="comment" required><?php echo $editComment; ?></textarea>
               <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
             </div>
           </div>
           <!-- つぶやくボタン -->
-          <button type="submit" class="btn btn-primary col-xs-12" disabled>つぶやく</button>
+          <button type="submit" class="btn btn-primary col-xs-12" disabled><?php echo $btnStr; ?></button>
+          <input type="hidden" name="id" value="<?php echo $id; ?>">
         </form>
       </div>
 
@@ -130,13 +194,16 @@
           <?php foreach($posts as $post): ?>
             <article class="timeline-entry">
                 <div class="timeline-entry-inner">
+                  <a href="bbs.php?action=edit&id=<?php echo $post['id']; ?>">
                     <div class="timeline-icon bg-success">
                         <i class="entypo-feather"></i>
                         <i class="fa fa-cogs"></i>
                     </div>
+                  </a>
                     <div class="timeline-label">
                         <h2><a href="#"><?php echo htmlspecialchars($post['nickname']); ?></a> <span><?php echo $post['created']; ?></span></h2>
                         <p><?php echo htmlspecialchars($post['comment']); ?></p>
+                        <a onclick="return confirm('本当に削除しますか？');" href="bbs.php?action=delete&id=<?php echo $post['id']; ?>"><i class="fa fa-trash trash"></i></a>
                     </div>
                 </div>
             </article>
